f8b4acaac91acececdebe5944ac75b1f418d2d49
Vlaimir Korba <krissky1986@gmail.com>
2024-08-19 19:38:48 +0300

    #34_TodoApp_Part_7_CSS_Stylesheets_To_Style_Components

--- src/components/InlineComponent.jsx
+++ src/components/InlineComponent.jsx
@@ -1,9 +1,9 @@
-const header = { color: "blue", fontSize: "140px" };
+import "../css/style.css";
 
 export default function InlineComponent() {
     return (
         <div>
-            <h1 style={header}>Inline Component</h1>
+            <h1 className="header">Inline Component</h1>
         </div>
     );
 }
