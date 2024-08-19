75b771b625e808abb1de5d010d5b8ed36af49dd4
Vlaimir Korba <krissky1986@gmail.com>
2024-08-19 19:32:29 +0300

    #33_Todo_App_Part_6_Inline_Styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import InlineComponent from "./components/InlineComponent";
 import Todo from "./components/Todo";
 
 function App() {
     return (
         <div className="App">
-            <Todo />
+            {/* <Todo /> */}
+            <InlineComponent />
         </div>
     );
 }
