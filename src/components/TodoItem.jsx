76e905d0d1e932ab1ccddb9e2d7941bf831db80c
Vlaimir Korba <krissky1986@gmail.com>
2024-08-21 12:34:08 +0300

    #39_Todo_App_Part_12_Styling_Todo_Items

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,7 +1,10 @@
+import styles from "./todoitem.module.css";
+
 export default function TodoItem({ item }) {
     return (
-        <div>
-            <h3>{item}</h3>
+        <div className={styles.item}>
+            <div className={styles.itemName}>{item}</div>
+            <hr className={styles.line} />
         </div>
     );
 }
