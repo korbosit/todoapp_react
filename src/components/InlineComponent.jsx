a17298a134d7418c91b9059dc76fe5fbbbfaccd9
Vlaimir Korba <krissky1986@gmail.com>
2024-08-21 10:10:06 +0300

    #36_Todo_App_Part_9_Creating_Header_and_Styling_It

--- src/components/InlineComponent.jsx
+++
@@ -1,9 +0,0 @@
-import styles from "./inlinecomponents.module.css";
-
-export default function InlineComponent() {
-    return (
-        <div>
-            <h1 className={styles.header}>Inline Component</h1>
-        </div>
-    );
-}
