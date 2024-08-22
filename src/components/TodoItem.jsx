7d48ad0e4757d98543c808acfec1939a91b06ea5
Vlaimir Korba <krissky1986@gmail.com>
2024-08-22 15:36:15 +0300

    #43: Marking Todo Items As Complete
     #44: Counting Completed & Total Todos

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -2,13 +2,27 @@ import styles from "./todoitem.module.css";
 
 export default function TodoItem({ item, todos, setTodos }) {
     function handleDelete(item) {
-        console.log("Delete buton clicked for item", item);
+        // console.log("Delete buton clicked for item", item);
         setTodos(todos.filter((todo) => todo !== item));
     }
+    function handleClick(name) {
+        // console.log("Item text clicked", name);
+        const newArray = todos.map((todo) =>
+            todo.name === name ? { ...todo, done: !todo.done } : todo
+        );
+        setTodos(newArray);
+        // console.log(todos);
+    }
+    const className = item.done ? styles.completed : "";
     return (
         <div className={styles.item}>
             <div className={styles.itemName}>
-                {item.name}{" "}
+                <span
+                    className={className}
+                    onClick={() => handleClick(item.name)}
+                >
+                    {item.name}{" "}
+                </span>
                 <span>
                     <button
                         onClick={() => handleDelete(item)}
